dc78cbd0240f772d08b1bc8477f6078e929f63d3
SlothTM <tmatuluko@gmail.com>
2025-11-11 01:58:16 +0000

    Completed Tutorial Scene Base
    Completed tutorial scene switches between each tutorial at the press of the corresponding number.
    
    Need to add Everyones tutorial to their platform

---
+++ Assets/CameraSwithc.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class CameraSwithc : MonoBehaviour
+{
+    public GameObject[] platforms;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = new Vector3(0f, 6.66f, -11.19f);
+        foreach (GameObject platform in platforms)
+        {
+            platform.SetActive(false);
+        }
+
+        platforms[0].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            foreach (GameObject platform in platforms)
+            {
+                platform.SetActive(false);
+            }
+
+            transform.position = new Vector3(0f, 6.66f, -11.19f);
+            platforms[0].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            foreach (GameObject platform in platforms)
+            {
+                platform.SetActive(false);
+            }
+
+            transform.position = new Vector3(11f, 6.66f, -11.19f);
+            platforms[1].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            foreach (GameObject platform in platforms)
+            {
+                platform.SetActive(false);
+            }
+
+            transform.position = new Vector3(22f, 6.66f, -11.19f);
+            platforms[2].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            foreach (GameObject platform in platforms)
+            {
+                platform.SetActive(false);
+            }
+            transform.position = new Vector3(33f, 6.66f, -11.19f);
+            platforms[3].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            foreach (GameObject platform in platforms)
+            {
+                platform.SetActive(false);
+            }
+            transform.position = new Vector3(44f, 6.66f, -11.19f);
+            platforms[4].SetActive(true);
+        }
+    }
+}
--- Assets/RigidMove.cs
+++ Assets/RigidMove.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 public class RigidMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float speed = 0f;
+    public float maxSpeed = 100f;
+    public float acceleration = 10f;
+    public float rotation;
+
+    public Rigidbody rb;  
+    
     void Start()
     {
         
@@ -11,6 +19,35 @@ public class RigidMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(Input.GetKey("space"))
+        {
+           if(speed < maxSpeed)
+            {
+                speed += acceleration;
+            }
+
+        }
+
+
+        else
+        {
+            if (speed > 0)
+            {
+                speed -= acceleration;
+            }
+        }
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if(Input.GetKey("a"))
+        {
+            transform.Rotate(0, -0.5f, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 0.5f, 0);
+        }
     }
 }
